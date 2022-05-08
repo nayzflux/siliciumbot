51db3454de5b0a872a37de1ad6a3f463526b282b
NayZBySodium <nayzbysodium@users.noreply.github.com>
2022-05-08 11:22:38 +0200

    Autorole command

--- controller/autorole.controller.js
+++ controller/autorole.controller.js
@@ -4,27 +4,39 @@ const helper = require(`../helpers/helper`);
 const setAutorole = async (guildId, roleId, callback) => {
     if (await AutoroleModel.exists({ guild_id: guildId })) {
         const autorole = await AutoroleModel.findOneAndUpdate({ guild_id: guildId }, { role_id: roleId }, { new: true });
-        // resolve fetch role and guild
+        // resolve fetch role
         const role = await helper.getRoleById(autorole.role_id);
-        const guild = await helper.getGuildById(autorole.guild_id);
-        callback(false, { role: role, guild: guild });
+        callback(false, role);
     } else {
         const autorole = await AutoroleModel.create({ guild_id: guildId, role_id: roleId });
-        // resolve fetch role and guild
-        const role = await helper.getRoleById(autorole.role_id);
-        const guild = await helper.getGuildById(autorole.guild_id);
-        callback(false, { role: role, guild: guild });
+        // resolve fetch role
+        const role = await helper.getRoleById(autorole.guild_id, autorole.role_id);
+        callback(false, role);
     }
 }
 
 const getAutorole = async (guildId, callback) => {
     if (await AutoroleModel.exists({ guild_id: guildId })) {
         const autorole = await AutoroleModel.findOne({ guild_id: guildId });
-        // resolve fetch role and guild
-        const role = await helper.getRoleById(autorole.role_id);
-        const guild = await helper.getGuildById(autorole.guild_id);
-        callback(false, { role: role, guild: guild });
+        // resolve fetch role
+        const role = await helper.getRoleById(autorole.guild_id, autorole.role_id);
+        callback(false, role);
     } else {
         callback(true, null);
     }
+}
+
+const removeAutorole = async (guildId, callback) => {
+    if (await AutoroleModel.exists({ guild_id: guildId })) {
+        await AutoroleModel.findOneAndRemove({ guild_id: guildId });
+        callback(false);
+    } else {
+        callback(true);
+    }
+}
+
+module.exports = {
+    getAutorole,
+    setAutorole,
+    removeAutorole
 }
--- helpers/helper.js
+++ helpers/helper.js
@@ -1,18 +1,18 @@
 const { client } = require(`../index`);
 
 const getUserById = async (userId) => {
-    const user = await client.users.fetch(userId);
+    const user = await client.users.cache.find(uuser => ser.id === userId);
     return user;
 }
 
 const getRoleById = async (guildId, roleId) => {
-    const guild = await client.guilds.fetch(guildId).role;
-    const role = await guild.roles.fetch(roleId)
+    const guild = await getGuildById(guildId);
+    const role = await guild.roles.cache.find(role => role.id === roleId);
     return role;
 }
 
 const getGuildById = async (guildId) => {
-    const guild = await client.guilds.fetch(guildId)
+    const guild = await client.guilds.cache.find(g => g.id === guildId);
     return guild;
 }
 
